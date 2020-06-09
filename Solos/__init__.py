11a4af99bbdf3bba7060b50114b9ae2daf3a8a0c
Juan Montesinos <juanfelipe.montesinos@upf.edu>
2020-06-09 04:27:37 +0200

    Functional SKReader

--- Solos/__init__.py
+++ Solos/__init__.py
@@ -1,3 +1,5 @@
+PATH = __path__[0]
 from .json_files import *
 
 from .skeleton import SKReader
+
--- Solos/skeleton.py
+++ Solos/skeleton.py
@@ -2,26 +2,24 @@ import numpy as np
 import os
 
 from .utils import BaseDict
-
+from . import PATH
 __all__ = ['SKReader']
 
 
 class SKReader(object):
     def __init__(self, download=True, in_ram=False):
-        path = __path__[0]
-        path = os.path.join(path, 'skeleton_files', 'skeleton.npy')
-        self.json = BaseDict().load(path=os.path.join(path, 'skeleton_files', 'skeleton_dict.json'))
+        self.json = BaseDict().load(path=os.path.join(PATH, 'skeleton_files', 'skeleton_dict.json'))
         N = self.json['_counter']
-        self.npy = np.memmap(path, dtype=np.float32, mode='r', shape=(N, 3, 47))
-        if in_ram:
-            self.npy = self.npy.copy()
 
         if download:
             from google_drive_downloader import GoogleDriveDownloader as gdd
 
             gdd.download_file_from_google_drive(file_id='1QRn7KMoJVD342VjpxsQh_uyQPhH2859B',
-                                                dest_path=path,
+                                                dest_path=os.path.join(PATH,'skeleton_files','skeleton.npy'),
                                                 unzip=False)
+        self.npy = np.memmap(os.path.join(PATH, 'skeleton_files', 'skeleton.npy'), dtype=np.float32, mode='r', shape=(N, 3, 47))
+        if in_ram:
+            self.npy = self.npy.copy()
 
     def __call__(self, path, offset, length):
         return self.npy[offset:offset + length]
@@ -35,3 +33,5 @@ class SKReader(object):
 
     def __len__(self):
         return len(self.json)
+    def keys(self):
+        return self.json.keys()
